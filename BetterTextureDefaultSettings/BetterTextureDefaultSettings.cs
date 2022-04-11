5bc3343cdd4a441832e57898751b60772e6a2257
Kyuubi <kyuubi.no.yoko86@gmail.com>
2022-04-11 06:16:00 +0200

    Add ModConfig

--- BetterTextureDefaultSettings/BetterTextureDefaultSettings.cs
+++ BetterTextureDefaultSettings/BetterTextureDefaultSettings.cs
@@ -11,6 +11,11 @@ namespace BetterTextureDefaultSettings
 {
     public class BetterTextureDefaultSettings : NeosMod
     {
+        public static ModConfiguration Config;
+        
+        [AutoRegisterConfigKey]
+        private static ModConfigurationKey<TextureWrapMode> TextureWrapMode = new ModConfigurationKey<TextureWrapMode>("TextureWrapMode", "Default Texture Wrap Mode.", ()=> FrooxEngine.TextureWrapMode.Clamp);
+
         public override string Author => "Banane9";
         public override string Link => "https://github.com/Banane9/NeosBetterTextureDefaultSettings";
         public override string Name => "BetterTextureDefaultSettings";
@@ -19,6 +24,8 @@ namespace BetterTextureDefaultSettings
         public override void OnEngineInit()
         {
             Harmony harmony = new Harmony($"{Author}.{Name}");
+            Config = GetConfiguration();
+            Config.Save(true);
             harmony.PatchAll();
         }
 
@@ -29,8 +36,8 @@ namespace BetterTextureDefaultSettings
             [HarmonyPatch("OnAwake")]
             private static void Postfix(StaticTexture2D __instance)
             {
-                __instance.WrapModeU.Value = TextureWrapMode.Clamp;
-                __instance.WrapModeV.Value = TextureWrapMode.Clamp;
+                __instance.WrapModeU.Value = Config.GetValue(TextureWrapMode);
+                __instance.WrapModeV.Value = Config.GetValue(TextureWrapMode);
             }
         }
     }
